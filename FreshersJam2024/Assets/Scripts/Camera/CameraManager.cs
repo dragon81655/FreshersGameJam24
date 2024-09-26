8fda10788e3e070a46f1e2d7d1f234c6ee785e99
Nightmare020 <albalexbelen@gmail.com>
2024-09-27 00:28:58 +0100

    Camera Manager functionality

--- FreshersJam2024/Assets/Scripts/Camera/CameraManager.cs
+++ FreshersJam2024/Assets/Scripts/Camera/CameraManager.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     // Zoom level
-    private float zoom;
+    private float targetZoom;
+    private float defaultZoom;
 
     // Velocity of zoom
     private float zoomVelocity = 2f;
@@ -22,16 +23,24 @@ public class CameraManager : MonoBehaviour
     private float lastClickTime;
 
     // Flag to see if we've already zoom in
-    private bool HasZoomIn = false;
+    private bool HasZoomedIn = false;
+
+    // Flag to check if we're in the process of zooming in or out
+    private bool isZooming = false;
 
     // Camera reference
     [SerializeField]
-    private Camera cam = null;
+    private Camera cam;
+
+    // Target position for the camera to move
+    private Vector3 targetPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        defaultZoom = cam.orthographicSize;
+        targetZoom = defaultZoom;
+        targetPosition = cam.transform.position;
     }
 
     // Update is called once per frame
@@ -41,22 +50,48 @@ public class CameraManager : MonoBehaviour
         {
             CheckForDoubleClick();
         }
+
+        if (isZooming)
+        {
+            // Smoothly interpolate the camera's position and zoom level
+            cam.transform.position = Vector3.Lerp(cam.transform.position, targetPosition, smoothTransition * Time.deltaTime * zoomVelocity);
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, smoothTransition * Time.deltaTime * zoomVelocity);
+
+            // Check if the camera has reached the target position and zoom level
+            if (Vector3.Distance(cam.transform.position, targetPosition) < 0.01f && Mathf.Abs(cam.orthographicSize - targetZoom) < 0.01f)
+            {
+                // Stop zooming once the target is reached
+                isZooming = false;
+            }
+        }
     }
 
     void ZoomIn(BaseRoom room)
     {
         // Set the target position to the center of the room
-        Vector3 targetPosition = new Vector3(room.transform.position.x, room.transform.position.y, cam.transform.position.z);
+        targetPosition = new Vector3(room.transform.position.x, room.transform.position.y, cam.transform.position.z);
+
+        // Set the target zoom level to be closer
+        targetZoom = defaultZoom / 2f;
 
         // Set the zoom in flag to true
-        HasZoomIn = true;
+        HasZoomedIn = true;
+
+        // Start the zooming process
+        isZooming = true;
     }
 
     void ZoomOut()
     {
+        // reset to the original position and zoom
+        targetPosition = new Vector3(0, 0, cam.transform.position.z);
+        targetZoom = defaultZoom;
 
         // Set the zoom in flag to true
-        HasZoomIn = false;
+        HasZoomedIn = false;
+
+        // Start the zooming process
+        isZooming = true;
     }
 
     void CheckForDoubleClick()
@@ -75,7 +110,7 @@ public class CameraManager : MonoBehaviour
 
                 if (room != null) 
                 { 
-                    if (!HasZoomIn)
+                    if (!HasZoomedIn)
                     {
                         ZoomIn(room);
                     }
@@ -86,6 +121,9 @@ public class CameraManager : MonoBehaviour
                 }
           
             }
+
+            // Update the last click time
+            lastClickTime = Time.time;
         }
     }
 }
